1cb938d8cccefbc96d62b263fa9e1d331c9917a1
Mikolaj Wyskocki <mikolajwyskocki@gmail.com>
2025-10-29 12:12:10 +0100

    Updated home.tsx to use MiniDrawer component

--- my-react-router-app/app/routes/home.tsx
+++ my-react-router-app/app/routes/home.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "./+types/home";
-import Register from "~/components/Register";
+import MiniDrawer from "../components/Drawer.tsx";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +9,5 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Register />;
+  return <MiniDrawer />;
 }
